6718642df6ddac7fc0eacd62cc9b45379f9a5614
hiagobelem <hiagomb99@gmail.com>
2025-09-04 22:48:58 -0400

    Adição de imagem principal

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const botoes = document.querySelectorAll('.carrossel button');
+const panels = document.querySelectorAll('.panel');
+
+document.getElementById('home').classList.add('ativa');
+document.querySelector('[data-secao="home"]').classList.add('ativo');
 
 botoes.forEach(botao => {
   botao.addEventListener('click', () => {
     botoes.forEach(b => b.classList.remove('ativo'));
     botao.classList.add('ativo');
+    panels.forEach(p => p.classList.remove('ativa'));
+
+    const idSecao = botao.dataset.secao;
+    document.getElementById(idSecao).classList.add('ativa');
   });
 });
